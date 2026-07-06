1b1ffd4663b76575f4d10095a379e913ef9e96a9
Michael Angelo Rilan <michael.angelo.rilan@accenture.com>
2026-07-06 13:44:11 +0000

    initialized ui

---
+++ app/psa-monitoring-ui/webapp/controller/psa-monitoring-view.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("psamonitoringui.controller.psa-monitoring-view", {
+        onInit() {
+        }
+    });
+});
